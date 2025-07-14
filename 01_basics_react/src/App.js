a4d101ef81399510805c47d63e22ab138bd9f95f
AK <92323484+GITAK007@users.noreply.github.com>
2025-07-15 00:34:29 +0530

    some file are deleted and modified along with that .

---
+++ 01_basics_react/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+      <h1> React Ak Proj</h1>
+  );
+}
+
+export default App;
---
+++ 01_vite_react/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+      <h1> Hey this is a Vite-React App  |  AK DevFlows</h1>
+  )
+}
+
+export default App
